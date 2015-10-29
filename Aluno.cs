ae533cd663feeddcbb7ea162c31309ecb09fc347
matheushps <matheus_hps@hotmail.com>
2015-10-29 12:02:33 -0200

    Revert "Revert "Desafio Alunos""
    This reverts commit d79c5f5c6c38ece074ce234dd2e73c7e376ee10a.

---
+++ Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioAlunos
+{
+   public class Aluno : Grupo
+    {
+       public Aluno()
+       {
+       }
+
+        public string Nome { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
